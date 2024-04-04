71749a39adac07281841b721640da67d0da7e9c0
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-04 11:55:03 +0100

    got the poke data (but slow)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
+import pokemonArray from "./data/pokemon";
 import "./styles/style.scss";
+
+const cardContainer = document.querySelector(".card-container");
+console.log(cardContainer);
+
+if (!cardContainer) {
+  throw new Error("issue with selectors");
+}
+
+// need to run through the array, assign each pokemon to html values
+const handleRender = () => {
+  cardContainer.innerHTML = "";
+  pokemonArray.forEach((pokemon) => {
+    cardContainer.innerHTML += `
+		<div class="card">
+    	<img src="${pokemon.sprite}"class="card__image"/>
+			<div class="card__content">
+				<h1 class="card__heading">${pokemon.name}</h1>
+				<p class="card__text">${pokemon.types.join(", ")}</P>
+			 </div>
+		</div>`;
+  });
+};
+
+// EVENT LISTENER
+window.addEventListener("load", handleRender);
